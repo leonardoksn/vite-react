fdc50d72237d1b9914c7f7068a43d998a811ffc7
Leonardo Nunes <leonardo.nunes@grupokochsa.com.br>
2023-03-17 10:39:17 -0300

    PWA configurado

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,35 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: { https: true },
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'TMS e-commerce',
+        short_name: 'TMS Only',
+        description: 'Sistema de gerenciamento de transporte',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
