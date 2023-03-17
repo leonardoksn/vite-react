70f10be2530aaa0fdf0f515064aa9a1166f1e745
Leonardo Nunes <leonardo.nunes@grupokochsa.com.br>
2023-03-17 10:40:53 -0300

    Modulos criados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,10 @@
+import { Outlet } from 'react-router-dom';
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Outlet />
+    </>
+  );
 }
